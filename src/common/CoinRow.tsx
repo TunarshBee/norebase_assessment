2d6138a64ebe2e1c18b788a2aa9bd09f367c478c
TunarshBee <tunarsh4real@gmail.com>
2024-10-30 11:31:53 +0100

    [Fix]: generic table size

--- src/common/CoinRow.tsx
+++ src/common/CoinRow.tsx
@@ -5,6 +5,14 @@ const CoinRow: React.FC<{
   itemIdx: number;
   data: { [key: string]: string };
 }> = ({ header, itemIdx, data }) => {
+
+  const formattedTotalSupply = (Number(data["tsupply"]) || 0).toLocaleString(
+    undefined,
+    {
+      notation: "compact",
+      maximumFractionDigits: 2,
+    }
+  );
     
   const rowData = [
     {
@@ -12,17 +20,17 @@ const CoinRow: React.FC<{
       value: data["name"],
     },
     {
-        title: header[1],
+      title: header[1],
       value: data["symbol"],
     },
     {
-        title: header[2],
+      title: header[2],
       value: `$${data["price_usd"]}`,
     },
     {
-        title: header[3],
-      value: `${data["tsupply"]} ${data["symbol"]}`,
-    }
+      title: header[3],
+      value: `${formattedTotalSupply} ${data["symbol"]}`,
+    },
   ];
 
   return (
@@ -36,7 +44,7 @@ const CoinRow: React.FC<{
           {rowData.map(({ title, value }) => (
             <td key={title}>
               <h4 className="mobileHeader">{title}</h4>
-              <p className="min-w-fit">{value}</p>
+              <p className="flex flex-wrap w-[100px] break-words">{value}</p>
             </td>
           ))}
         </tr>
