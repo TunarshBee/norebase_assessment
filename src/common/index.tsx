4b3f4a715b889d14aecc600a85dc64f952b81acb
TunarshBee <tunarsh4real@gmail.com>
2024-10-30 11:46:36 +0100

    ...

--- src/common/index.tsx
+++ src/common/index.tsx
@@ -28,7 +28,7 @@ const Table: React.FC<ITableProps> = ({
           {isLoadingData ? (
             <tbody data-testid="skeleton" className="min-w-fit">
               {loadingStateSkeletonRow.map((_item, index) => (
-                <tr key={index} className="min-w-[270px]">
+                <tr key={index} className="min-w-[250px]">
                   {header.map((_item, index) => (
                     <td key={index}>
                       <Skeleton height="20px" width="70%" />
